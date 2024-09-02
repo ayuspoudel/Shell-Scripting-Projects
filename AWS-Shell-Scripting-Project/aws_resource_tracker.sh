5677fc774816e8a4cd75695c39cc2872bb94c66b
Ayush Poudel <ayushpoudel@usf.edu>
2024-09-02 19:28:49 -0400

    Add 'set -x' and echo print description lines

--- AWS-Shell-Scripting-Project/aws_resource_tracker.sh
+++ AWS-Shell-Scripting-Project/aws_resource_tracker.sh
@@ -19,17 +19,23 @@
 #          4. AWS IAM Users
 
 
+set -x
+
 #List S3 Buckets
+echo "List of S3 Buckets"
 aws s3 ls
 
 
 #List ec2 instances
+echo "List of ec2 instances"
 aws ec2 describe-instances
 
 
 #list aws lambda
+echo "List of aws lambda"
 aws lambda list-functions
 
 #List AWS IAM USers
+echo "List of IAM users"
 aws iam list-users
 
